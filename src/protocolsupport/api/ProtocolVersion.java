72e9871429c966de4eb94a62ca3a0e52bff6e4a0
WHR <msl0000023508@gmail.com>
2022-01-28 14:43:59 +0800

    Remove protocolsupport.api.ProtocolVersion$OrderId: the array returned by 'values' method is already in values declaration order

--- src/protocolsupport/api/ProtocolVersion.java
+++ src/protocolsupport/api/ProtocolVersion.java
@@ -13,38 +13,35 @@ import org.apache.commons.lang3.Validate;
 import gnu.trove.map.hash.TIntObjectHashMap;
 
 public enum ProtocolVersion {
-
-	MINECRAFT_FUTURE(-1, new OrderId(17)),
-	MINECRAFT_1_11_1(316, new OrderId(16), "1.11.2"),
-	MINECRAFT_1_11(315, new OrderId(15), "1.11"),
-	MINECRAFT_1_10(210, new OrderId(14), "1.10"),
-	MINECRAFT_1_9_4(110, new OrderId(13), "1.9.4"),
-	MINECRAFT_1_9_2(109, new OrderId(12), "1.9.2"),
-	MINECRAFT_1_9_1(108, new OrderId(11), "1.9.1"),
-	MINECRAFT_1_9(107, new OrderId(10), "1.9"),
-	MINECRAFT_1_8(47, new OrderId(9), "1.8"),
-	MINECRAFT_1_7_10(5, new OrderId(8), "1.7.10"),
-	MINECRAFT_1_7_5(4, new OrderId(7), "1.7.5"),
-	MINECRAFT_1_6_4(78, new OrderId(6), "1.6.4"),
-	MINECRAFT_1_6_2(74, new OrderId(5), "1.6.2"),
-	MINECRAFT_1_6_1(73, new OrderId(4), "1.6.1"),
-	MINECRAFT_1_5_2(61, new OrderId(3), "1.5.2"),
-	MINECRAFT_1_5_1(60, new OrderId(2), "1.5.1"),
-	MINECRAFT_1_4_7(51, new OrderId(1), "1.4.7"),
-	MINECRAFT_LEGACY(-1, new OrderId(0)),
-	UNKNOWN(-1, new OrderId(-1));
+	UNKNOWN(-1),
+	MINECRAFT_LEGACY(-1),
+	MINECRAFT_1_4_7(51, "1.4.7"),
+	MINECRAFT_1_5_1(60, "1.5.1"),
+	MINECRAFT_1_5_2(61, "1.5.2"),
+	MINECRAFT_1_6_1(73, "1.6.1"),
+	MINECRAFT_1_6_2(74, "1.6.2"),
+	MINECRAFT_1_6_4(78, "1.6.4"),
+	MINECRAFT_1_7_5(4, "1.7.5"),
+	MINECRAFT_1_7_10(5, "1.7.10"),
+	MINECRAFT_1_8(47, "1.8"),
+	MINECRAFT_1_9(107, "1.9"),
+	MINECRAFT_1_9_1(108, "1.9.1"),
+	MINECRAFT_1_9_2(109, "1.9.2"),
+	MINECRAFT_1_9_4(110, "1.9.4"),
+	MINECRAFT_1_10(210, "1.10"),
+	MINECRAFT_1_11(315, "1.11"),
+	MINECRAFT_1_11_1(316, "1.11.2"),
+	MINECRAFT_FUTURE(-1);
 
 	private final int id;
-	private final OrderId orderId;
 	private final String name;
 
-	ProtocolVersion(int id, OrderId orderid) {
-		this(id, orderid, null);
+	ProtocolVersion(int id) {
+		this(id, null);
 	}
 
-	ProtocolVersion(int id, OrderId orderId, String name) {
+	ProtocolVersion(int id, String name) {
 		this.id = id;
-		this.orderId = orderId;
 		this.name = name;
 	}
 
@@ -80,7 +77,7 @@ public enum ProtocolVersion {
 	 * @throws IllegalArgumentException if protocol versions use different protocol types
 	 */
 	public boolean isAfter(ProtocolVersion another) {
-		return orderId.compareTo(another.orderId) > 0;
+		return ordinal() > another.ordinal();
 	}
 
 	/**
@@ -90,7 +87,7 @@ public enum ProtocolVersion {
 	 * @throws IllegalArgumentException if protocol versions use different protocol types
 	 */
 	public boolean isAfterOrEq(ProtocolVersion another) {
-		return orderId.compareTo(another.orderId) >= 0;
+		return ordinal() >= another.ordinal();
 	}
 
 	/**
@@ -100,7 +97,7 @@ public enum ProtocolVersion {
 	 * @throws IllegalArgumentException if protocol versions use different protocol types
 	 */
 	public boolean isBefore(ProtocolVersion another) {
-		return orderId.compareTo(another.orderId) < 0;
+		return ordinal() < another.ordinal();
 	}
 
 	/**
@@ -110,7 +107,7 @@ public enum ProtocolVersion {
 	 * @throws IllegalArgumentException if protocol versions use different protocol types
 	 */
 	public boolean isBeforeOrEq(ProtocolVersion another) {
-		return orderId.compareTo(another.orderId) <= 0;
+		return ordinal() <= another.ordinal();
 	}
 
 	/**
@@ -144,24 +141,6 @@ public enum ProtocolVersion {
 		return version != null ? version : UNKNOWN;
 	}
 
-	private static final ProtocolVersion[] sorted_versions;
-	static {
-		//List<ProtocolVersion> sorted_version_list = new ArrayList<>(Arrays.asList(values()));
-		//List<ProtocolVersion> sorted_version_list = Arrays.asList(values());
-		ProtocolVersion[] versions = values();
-		List<ProtocolVersion> sorted_version_list = new ArrayList<>(versions.length);
-		for(ProtocolVersion v : versions) {
-			if(v.orderId.id < 0) continue;
-			sorted_version_list.add(v);
-		}
-		Collections.sort(sorted_version_list, new Comparator<ProtocolVersion>() {
-				public int compare(ProtocolVersion v1, ProtocolVersion v2) {
-					return v1.orderId.compareTo(v2.orderId);
-				}
-			});
-		sorted_versions = sorted_version_list.toArray(new ProtocolVersion[0]);
-	}
-
 	/**
 	 * Returns protocol version that is used by the game version released after game version used by this protocol
 	 * Returns null if next game version doesn't exist
@@ -169,9 +148,10 @@ public enum ProtocolVersion {
 	 * @throws IllegalArgumentException if protocol type is UNKNOWN
 	 */
 	public ProtocolVersion next() {
-		int nextVersionOrderId = orderId.id + 1;
-		if (nextVersionOrderId < sorted_versions.length) {
-			return sorted_versions[nextVersionOrderId];
+		ProtocolVersion[] versions = values();
+		int nextVersionOrderId = ordinal() + 1;
+		if (nextVersionOrderId < versions.length) {
+			return versions[nextVersionOrderId];
 		} else {
 			return null;
 		}
@@ -184,9 +164,10 @@ public enum ProtocolVersion {
 	 * @throws IllegalArgumentException if protocol type is UNKNOWN
 	 */
 	public ProtocolVersion previous() {
-		int previousVersionOrderId = orderId.id - 1;
+		ProtocolVersion[] versions = values();
+		int previousVersionOrderId = ordinal() - 1;
 		if (previousVersionOrderId >= 0) {
-			return sorted_versions[previousVersionOrderId];
+			return versions[previousVersionOrderId];
 		} else {
 			return null;
 		}
@@ -200,11 +181,12 @@ public enum ProtocolVersion {
 	 * @return all protocol versions that are between specified ones (inclusive)
 	 */
 	public static ProtocolVersion[] getAllBetween(ProtocolVersion one, ProtocolVersion another) {
-		int startId = Math.min(one.orderId.id, another.orderId.id);
-		int endId = Math.max(one.orderId.id, another.orderId.id);
+		ProtocolVersion[] versions = values();
+		int startId = Math.min(one.ordinal(), another.ordinal());
+		int endId = Math.max(one.ordinal(), another.ordinal());
 		ProtocolVersion[] between = new ProtocolVersion[(endId - startId) + 1];
 		for (int i = startId; i <= endId; i++) {
-			between[i - startId] = sorted_versions[i];
+			between[i - startId] = versions[i];
 		}
 		return between;
 	}
@@ -251,21 +233,6 @@ public enum ProtocolVersion {
 	@Deprecated
 	public static ProtocolVersion[] getAllBefore(ProtocolVersion version) {
 		return getAllBetween(getOldest(), version);
-	}
-
-	private static class OrderId implements Comparable<OrderId> {
-
-		private final int id;
-
-		public OrderId(int id) {
-			this.id = id;
-		}
-
-		@Override
-		public int compareTo(OrderId o) {
-			return Integer.compare(id, o.id);
-		}
-
 	}
 
 }
