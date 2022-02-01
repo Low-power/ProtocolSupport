0ab7b1cbde4e7744735b14874497cc4764f6cb24
WHR <msl0000023508@gmail.com>
2022-02-01 17:42:14 +0800

    Improve command implementation

--- src/protocolsupport/commands/CommandHandler.java
+++ src/protocolsupport/commands/CommandHandler.java
@@ -16,6 +16,7 @@ import io.netty.util.ResourceLeakDetector.Level;
 import protocolsupport.api.ProtocolSupportAPI;
 import protocolsupport.api.ProtocolVersion;
 import protocolsupport.zplatform.ServerPlatform;
+import protocolsupport.zplatform.PlatformUtils;
 
 public class CommandHandler implements CommandExecutor, TabCompleter {
 
@@ -25,38 +26,67 @@ public class CommandHandler implements CommandExecutor, TabCompleter {
 			sender.sendMessage(ChatColor.RED + "You have no power here!");
 			return true;
 		}
-		if ((args.length == 1) && args[0].equalsIgnoreCase("list")) {
+		if(args.length < 1) return false;
+		if(args[0].equals("list")) {
+			if(args.length > 2) return false;
+			boolean should_list_all = false;
+			if(args.length == 2) {
+				if(args[1].equals("-a")) should_list_all = true;
+				else return false;
+			}
 			for (ProtocolVersion version : ProtocolVersion.values()) {
-				if (version.getName() != null) {
-					sender.sendMessage(ChatColor.GOLD+"["+version.getName()+"]: "+ChatColor.GREEN+getPlayersStringForProtocol(version));
+				String name = version.getName();
+				if(name == null) continue;
+				String players = getPlayersStringForProtocol(version);
+				if(!should_list_all && players.isEmpty()) continue;
+				sender.sendMessage(String.format("%s[%s]%s %s%s%s",
+					ChatColor.GOLD, name, ChatColor.RESET,
+					ChatColor.GREEN, players, ChatColor.RESET));
+			}
+		} else if(args[0].equals("debug")) {
+			if(args.length > 2) return false;
+			PlatformUtils utils = ServerPlatform.get().getMiscUtils();
+			if(args.length == 2) {
+				if(args[1].equalsIgnoreCase("off")) {
+					utils.disableDebug();
+				} else if(args[1].equalsIgnoreCase("on")) {
+					utils.enableDebug();
+				} else {
+					return false;
 				}
-			}
-			return true;
-		}
-		if ((args.length == 1) && args[0].equalsIgnoreCase("debug")) {
-			if (ServerPlatform.get().getMiscUtils().isDebugging()) {
-				ServerPlatform.get().getMiscUtils().disableDebug();
-				sender.sendMessage(ChatColor.GOLD + "Disabled debug");
+			} else if(utils.isDebugging()) {
+				utils.disableDebug();
 			} else {
-				ServerPlatform.get().getMiscUtils().enableDebug();
-				sender.sendMessage(ChatColor.GOLD + "Enabled debug");
+				utils.enableDebug();
 			}
-			return true;
-		}
-		if ((args.length == 1) && args[0].equalsIgnoreCase("leakdetector")) {
-			if (ResourceLeakDetector.isEnabled()) {
+			sender.sendMessage(ChatColor.GOLD.toString() + 
+				(utils.isDebugging() ? "Enabled" : "Disabled") + " debug");
+		} else if(args[0].equals("leakdetector")) {
+			if(args.length > 2) return false;
+			if(args.length == 2) {
+				if(args[1].equalsIgnoreCase("off")) {
+					ResourceLeakDetector.setLevel(Level.DISABLED);
+				} else if(args[1].equalsIgnoreCase("on")) {
+					ResourceLeakDetector.setLevel(Level.PARANOID);
+				} else {
+					return false;
+				}
+			} else if (ResourceLeakDetector.isEnabled()) {
 				ResourceLeakDetector.setLevel(Level.DISABLED);
-				sender.sendMessage(ChatColor.GOLD + "Disabled leak detector");
 			} else {
 				ResourceLeakDetector.setLevel(Level.PARANOID);
-				sender.sendMessage(ChatColor.GOLD + "Enabled leak detector");
 			}
-			return true;
+			sender.sendMessage(ChatColor.GOLD.toString() +
+				(ResourceLeakDetector.isEnabled() ? "Enabled" : "Disabled") +
+				" leak detector");
+		} else {
+			sender.sendMessage("Invalid subcommand");
+			return false;
 		}
-		return false;
+		return true;
 	}
 
-	private String getPlayersStringForProtocol(ProtocolVersion version) {
+	private static String getPlayersStringForProtocol(ProtocolVersion version) {
 		StringBuilder sb = new StringBuilder();
 		for (Player player : Bukkit.getOnlinePlayers()) {
 			if (ProtocolSupportAPI.getProtocolVersion(player) == version) {
